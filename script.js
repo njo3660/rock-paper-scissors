e15df92c374f55a01e2f564775ab2e80ce397221
Nik <olivieri.nikolas@gmail.com>
2022-11-09 17:01:22 -0500

    Write code for the playRound function.

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 function getComputerChoice() {
     let rand = Math.random();
     if (rand < .33){
-        return "Rock";
+        return "rock";
     }
     else if (rand > .65){
-        return "Scissors";
+        return "scissors";
     }
     else {
-        return "Paper";
+        return "paper";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    let pSel = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1);
+    let cSel = computerSelection.slice(0,1).toUpperCase() + computerSelection.slice(1);
+
+    switch(playerSelection) {
+        case "rock":
+            switch(computerSelection) {
+                case "scissors":
+                    return `You Win! ${pSel} beats ${cSel}`;
+                case "paper":
+                    return `You Lose! ${cSel} beats ${pSel}`;
+                default:
+                    return "It's a Draw!";
+            }
+        case "paper":
+            switch(computerSelection) {
+                case "rock":
+                    return `You Win! ${pSel} beats ${cSel}`;
+                case "scissors":
+                    return `You Lose! ${cSel} beats ${pSel}`;
+                default:
+                    return "It's a Draw!";
+            }
+        case "scissors":
+            switch(computerSelection) {
+                case "paper":
+                    return `You Win! ${pSel} beats ${cSel}`;
+                case "rock":
+                    return `You Lose! ${cSel} beats ${pSel}`;
+                default:
+                    return "It's a Draw!";
+            }
+        default:
+            return "Something went wrong...";
     }
 }
